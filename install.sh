1ce2203529dbafb845f4c51ca6c711b73e98da37
Max Penet <m@qbits.cc>
2014-02-02 10:10:31 +0100

    add super simple installer

---
+++ install.sh
@@ -0,0 +1,17 @@
+if [ "$#" -lt 1 ]; then 	# -n tests to see if the argument is non empty
+	echo "Missing laptop name"
+    exit 1
+fi
+
+DIR=$(cd $(dirname "$0"); pwd)
+
+sudo apt-get install xmonad trayer dmenu xmobar -y
+
+mv /usr/share/xsessions/xmonad.start /usr/share/xsessions/xmonad.start.original
+
+ln -s $DIR/.xmonad ~/.xmonad
+ln -s $DIR/$1/.xmobarrc ~/.xmobarrc
+ln -s $DIR/.Xresources ~/.Xresources
+ln -s $DIR/.Xresources ~/.Xdefaults
+
+echo "[Desktop Entry]\nEncoding=UTF-8\nName=XMonad.mpenet\nComment=Lightweight tiling window manager\nExec=$DIR/$1/xmonad.start\nIcon=xmonad.png\nType=XSession" > /usr/share/xsessions/xmonad-$1.desktop ;
