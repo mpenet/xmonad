70826325a9c6cb679418189486cba7e52aecfbfb
Max Penet <m@qbits.cc>
2015-07-27 09:55:37 +0200

    unbork jvisualvm

--- .xmonad/xmonad.hs
+++ .xmonad/xmonad.hs
@@ -5,6 +5,7 @@ import XMonad.Util.Run(spawnPipe)
 import XMonad.Util.EZConfig
 import XMonad.Actions.NoBorders
 import XMonad.Layout.NoBorders
+import XMonad.Hooks.SetWMName
 import System.IO
 
 myManageHook = composeAll
@@ -15,6 +16,7 @@ main = do
     xmonad $ defaultConfig
         { manageHook = manageDocks <+> myManageHook
                         <+> manageHook defaultConfig
+        , startupHook = setWMName "LG3D"
         , layoutHook = avoidStruts $ smartBorders $ layoutHook defaultConfig
         , logHook = dynamicLogWithPP $ xmobarPP
                         { ppOutput = hPutStrLn xmproc
